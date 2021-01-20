e7e3c65035fc2c2689852b1e51d74312d6446f7d
Azuki-bar <42642269+Azuki-bar@users.noreply.github.com>
2021-01-20 19:40:19 +0900

    add frac

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-
-int main(void){
-printf("Hello world!");
-
+int fraction(int n) {
+    if (n < 2) {
+        return 1;
+    } else {
+        return (n * fraction(n - 1))
+    }
+}
+int main(void) {
+    printf("Hello world!\n");
+    printf("%d\n", fraction(10));
 }
