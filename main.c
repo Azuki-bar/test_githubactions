6c2c712f6f202a37772fa985fb587023e29a2956
Clang Robot <robot@example.com>
2021-01-23 08:43:51 +0000

    Committing clang-format changes

--- main.c
+++ main.c
@@ -4,4 +4,7 @@ int hoge(void) {
   printf("hoge\n");
   printf("fuga\n");
 }
-int main(void) { printf("Hello world!");printf("Hello world!"); }
+int main(void) {
+  printf("Hello world!");
+  printf("Hello world!");
+}
